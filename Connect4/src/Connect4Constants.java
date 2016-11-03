5920c07e7bc63c8dc4c0b789cc82203eba2e88bb
Eric Charnesky <eric.charnesky@careevolution.com>
2016-11-03 19:52:25 -0400

    Connect4
    Modified from
    http://www.cs.armstrong.edu/liang/intro11e/html/TicTacToeServer.html
    and http://www.cs.armstrong.edu/liang/intro11e/html/TicTacToeClient.html

---
+++ Connect4/src/Connect4Constants.java
@@ -0,0 +1,11 @@
+
+public interface Connect4Constants {
+	public final int PLAYER1 = 1;
+	public final int PLAYER2 = 2;
+	public final int PLAYER1_WON = 1;
+	public final int PLAYER2_WON = 2;
+	public final int DRAW = 3;
+	public final int CONTINUE = 4;
+	public final int BOARD_ROWS = 6;
+	public final int BOARD_COLUMNS = 7;
+}
