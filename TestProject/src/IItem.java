c67a55b6bd59fb191830985e016a8541bf0b13e1
Eric Charnesky <eric.charnesky@careevolution.com>
2016-11-10 19:50:12 -0500

    Adding Room and IItem

---
+++ TestProject/src/IItem.java
@@ -0,0 +1,5 @@
+
+public interface IItem {
+public String GetName();
+
+}
---
+++ TestProject/src/Room.java
@@ -0,0 +1,11 @@
+
+public class Room {
+	private IItem item;
+	private String description;
+	
+	public Room()
+	{
+		description = "Some room";
+		
+	}
+}
